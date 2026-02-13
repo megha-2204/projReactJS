66206e442cf38204018b6590180971f2df9033c9
Megha <megha@Meghas-MacBook-Air.local>
2026-02-13 08:44:22 +0530

    Handling form inputs

--- tasktrek001/src/components/TaskForm.jsx
+++ tasktrek001/src/components/TaskForm.jsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TaskForm.css"
 import Tag from "./Tag.jsx";
 
 const TaskForm = () => {
+    // const [task, setTask] = useState("");
+    // const [status, setStatus] = useState("todo");
+    //
+    // const handleTask = (e) => {
+    //     setTask(e.target.value)
+    // };
+    // const handleStatus = (e) => {
+    //     setStatus(e.target.value)
+    // };
+    // console.log(task);
+    // console.log(status);
+
+    const [taskData, setTaskData] = useState({
+        task: " ",
+        status: "todo"
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(taskData)
+    };
+
+    const handleChange = (e) => {
+        // const name = e.target.name;
+        // const value = e.target.value;
+        const {name, value} = e.target;
+        //console.log(name,value);
+        setTaskData(prev => {
+            return{...prev, [name]:value}
+        });
+    };
+
     return(
         <header className='app_header'>
-            <form>
-                <input type="text" className="task_input" placeholder="Enter your task"/>
+            <form onSubmit={handleSubmit}>
+                <input type="text" className="task_input" placeholder="Enter your task"
+                    name = "task" onChange={(e) => handleChange(e)}
+                />
                 <div className="task_form_bottom_line">
                     <div>
                         <Tag tagName = "HTML"/>
@@ -15,7 +49,7 @@ const TaskForm = () => {
                         <Tag tagName = "REACT" />
                     </div>
                     <div>
-                    <select className="task_status">
+                    <select className="task_status" name = "status" onChange={(e)=>{handleChange(e)}}>
                         <option value="todo">To do</option>
                         <option value="doing">Doing</option>
                         <option value="done">Done</option>
